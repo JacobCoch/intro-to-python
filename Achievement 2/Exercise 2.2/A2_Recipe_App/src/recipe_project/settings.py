c3aab1e2071a1144f5a1612f8f2eb8675ae201bb
JarJarDinks <Cochranejacob22@gmail.com>
2023-10-04 16:37:34 -0700

    finish task2.2 and add config.py for db and secret

--- Achievement 2/Exercise 2.2/A2_Recipe_App/src/recipe_project/settings.py
+++ Achievement 2/Exercise 2.2/A2_Recipe_App/src/recipe_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from config import DATABASES, SECRET_KEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-h*lvn1*wwi6i&ek=pgw14e(ec7v-=73c13et7(g)*^&iwx#7@&"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -72,17 +74,6 @@ WSGI_APPLICATION = "recipe_project.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "recipe-app",
-        "USER": "postgres",
-        "PASSWORD": "Malparidoguason22$",
-        "HOST": "localhost",
-        "PORT": "",
-    }
-}
 
 
 # Password validation
